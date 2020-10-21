87e492c5f62fcc7fcf0799fe18368603270a5125
Killian-drh <killian.deroche@hotmail.com>
2020-10-21 15:08:32 +0200

    add multi-dimensional array

--- morpion.go
+++ morpion.go
@@ -7,11 +7,33 @@ import (
   "strconv"
 )
 
+
 func main() {
   userInput := inputUser()
-  fmt.Println(userInput)
+  grid(userInput)
+
 }
 
+func grid(value int) {
+
+  var table = [3][3]int{ {1,2,3}, {4,5,6}, {7,8,9}}
+
+  var i, j int
+
+  /* output each array element's value */
+  for  i = 0; i < 3; i++ {
+     for j = 0; j < 3; j++ {
+       fmt.Printf("%d ",table[i][j] )
+       if j == 2{
+        fmt.Printf("\n")
+        }
+     }
+  }
+}
+
+
+
+// function input number in the grid
 func inputUser()int{
   scanner := bufio.NewScanner(os.Stdin)
   var input int
